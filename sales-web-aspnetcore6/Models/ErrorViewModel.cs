93ebff173e756377ab0309f25ea57a64a14501b3
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-12-09 22:05:37 -0300

    first commit

---
+++ sales-web-aspnetcore6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sales_web_aspnetcore6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
